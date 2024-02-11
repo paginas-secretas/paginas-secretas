bb0333cfb1c85c50c8b9cc72375396bc791d5b58
RuteSantos <1160663@isep.ipp.pt>
2024-02-11 21:09:43 +0000

    feat: complete contact interface

--- src/lib/models/contacts.ts
+++ src/lib/models/contacts.ts
@@ -2,4 +2,22 @@ export type ContactsList = Array<Contact>;
 
 export interface Contact {
 	name: string;
+	email: string;
+	phoneNumber: PhoneNumber;
+	gender: string;
+	birthDate: Date;
+}
+
+export interface Address {
+	type: string;
+	street: string;
+	location: string;
+	region: number;
+	country: string;
+	postalCode: string;
+}
+
+export interface PhoneNumber {
+	type: string;
+	value: string;
 }
