b8e52d7545d60582d6961c6ff07a249b062f1e30
RuteSantos <1160663@isep.ipp.pt>
2024-02-13 21:20:53 +0000

    refactor: use environment variables instead of hardcoded string for the contacts manager url and contacts endpoint

--- src/lib/http/client/contacts-manager-client.ts
+++ src/lib/http/client/contacts-manager-client.ts
@@ -1,8 +1,8 @@
 import { Client } from '.';
 
-// TODO
-const baseUrl = new URL('http://localhost:8787/');
-export const contactsEndpoint = 'contacts';
+const env = import.meta.env;
+const baseUrl = new URL(env.VITE_CONTACTS_MANAGER_URL);
+export const contactsEndpoint = env.VITE_CONTACTS_MANAGER_CONTACTS_ENDPOINT;
 
 export const docHashHeader = 'x-doc-hash';
 
