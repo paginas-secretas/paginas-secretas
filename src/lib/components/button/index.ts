59c3a4b4ccb0ef191f0af784aa72bbb38cd9164e
RuteSantos <1160663@isep.ipp.pt>
2024-02-13 20:54:35 +0000

    feat: add button to save contacts list

--- src/lib/components/button/index.ts
+++ src/lib/components/button/index.ts
@@ -1,2 +1,3 @@
 export { default as CreateContactsListButton } from './create-contacts-list-button.svelte';
 export { default as NewContactButton } from './new-contact-button.svelte';
+export { default as SaveContactsListButton } from './save-contacts-list-button.svelte';
