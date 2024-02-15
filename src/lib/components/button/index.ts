aa539e2f8a176ff0ce24423c9012f31687cc32ce
RuteSantos <1160663@isep.ipp.pt>
2024-02-15 18:40:06 +0000

    feat: create 'plus' button

--- src/lib/components/button/index.ts
+++ src/lib/components/button/index.ts
@@ -1,3 +1,4 @@
 export { default as CreateContactsListButton } from './create-contacts-list-button.svelte';
 export { default as NewContactButton } from './new-contact-button.svelte';
 export { default as SaveContactsListButton } from './save-contacts-list-button.svelte';
+export { default as PlusButton } from './plus-button.svelte';
---
+++ src/lib/components/button/plus-button.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let onClick: () => void;
+</script>
+
+<div class="flex items-center" aria-hidden="true" on:click={onClick}>
+	<svg width="24" height="24" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+		<path
+			fill="currentColor"
+			d="M480 480V128a32 32 0 0 1 64 0v352h352a32 32 0 1 1 0 64H544v352a32 32 0 1 1-64 0V544H128a32 32 0 0 1 0-64z"
+		/>
+	</svg>
+</div>
