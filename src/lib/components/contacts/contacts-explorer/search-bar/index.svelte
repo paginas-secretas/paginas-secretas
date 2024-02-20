ebef6bc7ad10d4afa723a9cc8979ff6f423919e5
freitzzz <1160907@isep.ipp.pt>
2024-02-20 21:36:31 +0000

    style(components): minor styling adjustments

--- src/lib/components/contacts/contacts-explorer/search-bar/index.svelte
+++ src/lib/components/contacts/contacts-explorer/search-bar/index.svelte
@@ -7,18 +7,10 @@
 		class="relative flex items-center w-full h-12 rounded-3xl focus-within:shadow-lg overflow-hidden bg-gray-800"
 	>
 		<div class="grid place-items-center h-full w-12 ml-2 text-white">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				class="h-6 w-6"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
-			>
+			<svg width="24" height="24" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+					fill="currentColor"
+					d="M8.195 0c4.527 0 8.196 3.62 8.196 8.084a7.989 7.989 0 0 1-1.977 5.267l5.388 5.473a.686.686 0 0 1-.015.98a.71.71 0 0 1-.993-.014l-5.383-5.47a8.23 8.23 0 0 1-5.216 1.849C3.67 16.169 0 12.549 0 8.084C0 3.62 3.67 0 8.195 0m0 1.386c-3.75 0-6.79 2.999-6.79 6.698c0 3.7 3.04 6.699 6.79 6.699s6.791-3 6.791-6.699c0-3.7-3.04-6.698-6.79-6.698"
 				/>
 			</svg>
 		</div>
