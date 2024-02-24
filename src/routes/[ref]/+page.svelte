1c1e493e2ea18ab3df3b6592ff9d89a2e8185e64
freitzzz <1160907@isep.ipp.pt>
2024-02-24 16:02:09 +0000

    feat(routing): add routing for opening shared contacts list

---
+++ src/routes/[ref]/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { ModalForm, ContactsViewer, onNextTick } from '@components';
+	import { ContactsListStore, createNewContactStore } from '@stores';
+	import { LL } from '@i18n';
+	import { onMount } from 'svelte';
+
+	const contactsListStore = ContactsListStore;
+	const newContactStore = createNewContactStore($LL);
+
+	const triggerCreateContactsList = function () {
+		contactsListStore.triggerCreateList();
+	};
+
+	const triggerStoreContactsList = function () {
+		contactsListStore.triggerStoreContactsList();
+	};
+
+	$: showNewContactModalForm = false;
+	$: contactsList = $contactsListStore.value;
+
+	onMount(triggerCreateContactsList);
+</script>
+
+404
---
+++ src/routes/[ref]/[hash]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { ContactsListStore } from '@stores';
+	import { onMount } from 'svelte';
+
+	const { ref, hash } = $page.params;
+	const contactsListStore = ContactsListStore;
+
+	onMount(() => contactsListStore.triggerFetchContactsList(ref, hash));
+</script>
+
+Olá!!!
+
+{ref}
+{hash}
