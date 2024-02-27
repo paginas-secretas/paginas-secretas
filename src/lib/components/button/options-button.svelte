44d9b8efe06eed55ec15df194673fb9edf1ffdeb
RuteSantos <1160663@isep.ipp.pt>
2024-02-27 20:39:16 +0000

    fix: show click icon when hovering options menu

--- src/lib/components/button/options-button.svelte
+++ src/lib/components/button/options-button.svelte
@@ -5,7 +5,11 @@
 	$: active = false;
 </script>
 
-<div class="flex items-center" on:click={() => (active = !active)} aria-hidden="true">
+<div
+	class="flex items-center cursor-pointer"
+	on:click={() => (active = !active)}
+	aria-hidden="true"
+>
 	<div>
 		<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24">
 			<path
