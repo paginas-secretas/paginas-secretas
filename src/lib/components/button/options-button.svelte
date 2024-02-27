b744db137ff91c6a92dfd7b492918e2add2d02bc
RuteSantos <1160663@isep.ipp.pt>
2024-02-27 21:38:45 +0000

    enhancement: hide options menu if clicking outside dropdown (second solution)

--- src/lib/components/button/options-button.svelte
+++ src/lib/components/button/options-button.svelte
@@ -1,35 +1,53 @@
 <script lang="ts">
 	export let values: string[];
 	export let onClick: (value: string) => void;
+	let container: HTMLElement;
+
+	$: active = false;
 </script>
 
-<div class="dropdown-container justify-center">
-	<div class="dropdown">
-		<button>
-			<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24">
-				<path
-					fill="currentColor"
-					fill-rule="evenodd"
-					d="M10.5 5A1.5 1.5 0 0 1 12 3.5h.01a1.5 1.5 0 0 1 1.5 1.5v.01a1.5 1.5 0 0 1-1.5 1.5H12a1.5 1.5 0 0 1-1.5-1.5V5Zm0 7a1.5 1.5 0 0 1 1.5-1.5h.01a1.5 1.5 0 0 1 1.5 1.5v.01a1.5 1.5 0 0 1-1.5 1.5H12a1.5 1.5 0 0 1-1.5-1.5V12Zm1.5 5.5a1.5 1.5 0 0 0-1.5 1.5v.01a1.5 1.5 0 0 0 1.5 1.5h.01a1.5 1.5 0 0 0 1.5-1.5V19a1.5 1.5 0 0 0-1.5-1.5H12Z"
-					clip-rule="evenodd"
-				/>
-			</svg>
-		</button>
-		<div class="dropdown">
-			<div class="dropdown-menu">
-				{#each values as value}
-					<div
-						tabindex="-1"
-						class="dropdown-item text-sm"
-						on:click={() => {
-							onClick(value);
-						}}
-						aria-hidden
-					>
-						{value}
-					</div>
-				{/each}
-			</div>
-		</div>
+<svelte:window
+	on:click={(e) => {
+		if (e.target instanceof Node && container.contains(e.target) == false) {
+			active = false;
+		}
+	}}
+/>
+
+<div
+	bind:this={container}
+	class="flex items-center"
+	on:click={() => (active = !active)}
+	aria-hidden="true"
+>
+	<div>
+		<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24">
+			<path
+				fill="currentColor"
+				fill-rule="evenodd"
+				d="M10.5 5A1.5 1.5 0 0 1 12 3.5h.01a1.5 1.5 0 0 1 1.5 1.5v.01a1.5 1.5 0 0 1-1.5 1.5H12a1.5 1.5 0 0 1-1.5-1.5V5Zm0 7a1.5 1.5 0 0 1 1.5-1.5h.01a1.5 1.5 0 0 1 1.5 1.5v.01a1.5 1.5 0 0 1-1.5 1.5H12a1.5 1.5 0 0 1-1.5-1.5V12Zm1.5 5.5a1.5 1.5 0 0 0-1.5 1.5v.01a1.5 1.5 0 0 0 1.5 1.5h.01a1.5 1.5 0 0 0 1.5-1.5V19a1.5 1.5 0 0 0-1.5-1.5H12Z"
+				clip-rule="evenodd"
+			/>
+		</svg>
 	</div>
 </div>
+
+{#if active}
+	<div class="dropdown dropdown-open">
+		<div class="dropdown-menu">
+			{#each values as value}
+				<div
+					tabindex="-1"
+					class="dropdown-item text-sm"
+					on:click={() => {
+						onClick(value);
+						active = false;
+					}}
+					aria-hidden
+				>
+					{value}
+				</div>
+			{/each}
+		</div>
+	</div>
+{/if}
