ca263840f55a93830e447958c560bbbab91bbf78
freitzzz <1160907@isep.ipp.pt>
2024-03-02 19:47:44 +0000

    feat(states): disable search bar and options menu if no contact records exist

--- src/lib/components/contacts/contacts-explorer/index.svelte
+++ src/lib/components/contacts/contacts-explorer/index.svelte
@@ -10,22 +10,32 @@
 	export let onShareSelected: () => void;
 	export let onGenerateKeyPairSelected: () => void;
 
-	const options = new Map([
-		[$LL.share().toString(), onShareSelected],
-		[$LL.generateKeyPair().toString(), onGenerateKeyPairSelected]
-	]);
-
 	$: searchValue = '';
 	$: regex = new RegExp(searchValue, 'i');
 	$: contacts = contactsList.filter((c) => regex.test(c.name));
+	$: disabled = contactsList.length === 0;
+	$: options = [
+		{
+			option: $LL.share().toString(),
+			callback: onShareSelected,
+			disabled: disabled
+		},
+		{
+			option: $LL.generateKeyPair().toString(),
+			callback: onGenerateKeyPairSelected,
+			disabled: false
+		}
+	];
 </script>
 
 <div class="flex flex-col grow gap-4 my-4">
 	<div class="flex flex-row justify-between items-center">
 		<div class="flex-grow mx-4">
-			<SearchBar bind:value={searchValue} />
+			<SearchBar bind:value={searchValue} {disabled} />
 		</div>
-		<OptionsButton values={[...options.keys()]} onClick={(value) => options.get(value)?.()} />
+		<OptionsButton
+			values={options.filter((x) => !x.disabled).map((x) => x.option) }
+			onClick={(value) => options.find((x) => x.option === value)?.callback()} />
 	</div>
 	<ContactList contactsList={contacts} {onContactSelected} />
 </div>
