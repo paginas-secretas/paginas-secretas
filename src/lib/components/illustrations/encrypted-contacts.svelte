59b6cc3ff8facb7d0b319d9e492cefb05eba725f
freitzzz <1160907@isep.ipp.pt>
2024-03-03 15:36:33 +0000

    feat(components): implement encrypted contacts illustration

---
+++ src/lib/components/illustrations/encrypted-contacts.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { LockedBook } from './images';
+	import { LL } from '@i18n';
+</script>
+
+<div class="flex flex-col items-center gap-4">
+	<div class="opacity-60 grayscale">
+		<LockedBook size="m" />
+	</div>
+
+	<p class="text-lg">{$LL.states.encryptedContacts()}</p>
+</div>
--- src/lib/components/illustrations/index.ts
+++ src/lib/components/illustrations/index.ts
@@ -2,4 +2,4 @@ export { default as NoContactRecords } from './no-contact-records.svelte';
 export { default as NoContactsFound } from './no-contacts-found.svelte';
 export { default as UnexpectedFailure } from './unexpected-failure.svelte';
 export { default as KeyGenerationFailure } from './key-generation-failure.svelte';
-export { default as Logo } from './images/logo.svelte';
+export { default as EncryptedContacts } from './encrypted-contacts.svelte';
