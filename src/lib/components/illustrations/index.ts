cce871e0d2ab684a47102c6b261faee136e59f24
freitzzz <1160907@isep.ipp.pt>
2024-03-09 11:37:51 +0000

    feat(components): add mobile layout not supported illustration

--- src/lib/components/illustrations/index.ts
+++ src/lib/components/illustrations/index.ts
@@ -3,3 +3,4 @@ export { default as NoContactsFound } from './no-contacts-found.svelte';
 export { default as UnexpectedFailure } from './unexpected-failure.svelte';
 export { default as KeyGenerationFailure } from './key-generation-failure.svelte';
 export { default as EncryptedContacts } from './encrypted-contacts.svelte';
+export { default as MobileLayoutNotSupported } from './mobile-layout-not-supported.svelte';
