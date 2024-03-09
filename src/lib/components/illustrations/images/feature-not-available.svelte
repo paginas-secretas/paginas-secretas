21584a65daf0e338ea77db77d79b87fe00f7bf27
freitzzz <1160907@isep.ipp.pt>
2024-03-09 11:37:35 +0000

    feat(components): add feature not available image

---
+++ src/lib/components/illustrations/images/feature-not-available.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Format, Size } from './type';
+	import { StaticImage } from './index';
+
+	export let size: Size = 'm';
+	export let format: Format = 'webp';
+</script>
+
+<StaticImage
+	name="feature-not-available"
+	alt="an illustration of a man trying to call someone"
+	{size}
+	{format}
+/>
