6e11e24bd97221b3bb4392e82bccb35c3b8e6e9c
freitzzz <1160907@isep.ipp.pt>
2024-03-10 21:06:03 +0000

    Updates

--- _app/immutable/nodes/5.f86f0b32.js
+++
@@ -1,1 +0,0 @@
-import{d as e,s as a}from"../chunks/no-contacts-found.b0494fc4.js";import{l as t}from"../chunks/i18n-util.async.fad76af7.js";import{P as m}from"../chunks/_page.a7c3856b.js";const l=async()=>{const o=e();await t(o),a(o)},s=Object.freeze(Object.defineProperty({__proto__:null,load:l},Symbol.toStringTag,{value:"Module"}));export{m as component,s as universal};
