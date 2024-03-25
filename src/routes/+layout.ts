d3923192610e69612aafae4eea22e1c9685a868f
freitzzz <1160907@isep.ipp.pt>
2024-03-25 20:47:52 +0000

    bugfix: disable ssr to re-enable hot reload

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,7 @@
 import { dev } from '$app/environment';
 
 export const prerender = !dev;
+
+// Disable SSR completely for two reasons: we build for CSR and this also fixes hot-reload getting
+// stuck on cyclic dependencies files
+export const ssr = false;
